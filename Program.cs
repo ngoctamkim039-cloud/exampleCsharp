756ef4b4c899191bcd7b08407f228641272497f2
ngoctamkim039-cloud <ngoctamkim039@gmail.com>
2025-09-21 21:14:55 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Chào mừng bạn đến với trò chơi Câu Đố Toán Học!");
+        Console.Write("Nhập tên của bạn: ");
+        string playerName = Console.ReadLine();
+
+        Console.WriteLine($"Xin chào, {playerName}! Hãy bắt đầu nhé!");
+        // Ở các bước tiếp theo, bạn sẽ bổ sung chức năng câu hỏi toán học tại đây.
+    }
+}
